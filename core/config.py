63b60d3c6160754e33364f93ac1b2a1b80010099
Sudip Bose <sbose007ime.work@gmail.com>
2024-04-11 01:32:36 +0530

    heisenbergdb hello world

---
+++ core/config.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from pydantic import BaseSettings
+
+
+class EnvironmentType(str, Enum):
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+    TEST = "test"
+
+    
+class BaseConfig(BaseSettings):
+    class Config:
+        case_sensitive = True
+
+
+class Config(BaseConfig):
+    DEBUG: int = 0
+    DEFAULT_LOCALE: str = "en_US"
+    ENVIRONMENT: str = EnvironmentType.DEVELOPMENT
+    RELEASE_VERSION: str = "1.0"
+    DB_CONTAINER_IP_ADDRESS = "IPAddress"
+    MONGODB_URL: str =  f"mongodb://<{DB_CONTAINER_IP_ADDRESS}>:27017/"
+
+config: Config = Config()
