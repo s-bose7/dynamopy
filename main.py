6a5250fef4e2cdb52bf4617b2327234b1cdf38c9
Sudip Bose <sbose007ime.work@gmail.com>
2024-05-17 16:59:25 +0530

    app gateway

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import uvicorn
 
+from core.config import config
 
 if __name__ == "__main__":
     print("heisenberg DB")
+    server_config = uvicorn.Config(
+        app="core.server:app",
+        host="localhost",
+        port=5000,
+        reload=False if config.ENVIRONMENT == "production" else True,
+        workers=1,
+    )
+    server = uvicorn.Server(config=server_config)
+    server.run()
