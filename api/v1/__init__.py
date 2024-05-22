d415d1b65e02cb1b4bcf66b25da28adf7b976e62
Sudip Bose <sbose007ime.work@gmail.com>
2024-05-22 09:20:02 +0530

    new endpoint added

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from get import get_router
 from store import store_router
+from delete import delete_router
 
 v1_router = APIRouter()
-v1_router.include_router(router=get_router, prefix="/get")
 v1_router.include_router(router=store_router, prefix="/store")
+v1_router.include_router(router=get_router, prefix="/get")
+v1_router.include_router(router=delete_router, prefix="/delete")
---
+++ api/v1/delete.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+
+delete_router = APIRouter()
