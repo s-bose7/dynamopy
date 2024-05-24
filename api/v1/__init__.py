5a0f1640e9fdb4fe0a1688d70bbb40d33efa7d5e
Sudip Bose <sbose007ime.work@gmail.com>
2024-05-24 21:06:56 +0530

    Fixed import error

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from get import get_router
-from store import store_router
-from delete import delete_router
+from api.v1.get import get_router
+from api.v1.store import store_router
+from api.v1.delete import delete_router
 
 v1_router = APIRouter()
 v1_router.include_router(router=store_router, prefix="/store")
